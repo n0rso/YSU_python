fa75ebb1df250815fd282626be9ca9c1ce481ef7
n0rso <58942678+n0rso@users.noreply.github.com>
2021-02-25 19:08:59 +0400

    Add files via upload

---
+++ home1/problem1.py
@@ -0,0 +1,6 @@
+import sys
+
+print("the given text: ",sys.argv[1])
+print("first word : ",sys.argv[2])
+print("second word:",sys.argv[3])
+print("Output string :",sys.argv[1].replace(sys.argv[2],sys.argv[3]))
